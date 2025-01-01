fbc87a6d35f53bb4bd9c14856eab3b9addb352c4
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2025-01-01 15:30:40 -0300

    Bea is better than another ai

---
+++ Bea/Bea/src/test/java/com/ai/Bea/BeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.Bea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
